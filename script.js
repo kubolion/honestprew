b6f5e4ff47b5f3943746780e7492b6e683c05108
kubolion <144912772+kubolion@users.noreply.github.com>
2023-11-14 17:03:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,23 +5,6 @@ menuBtn.addEventListener("click", () => {
   menuMobile.classList.toggle("menu--open");
 });
 
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
 
-  slidesPerView: 3,
-  spaceBetween: -20,
-  loop: true,
 
-  // If we need pagination
-  // pagination: {
-  //   el: ".swiper-pagination",
-  // },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  // And if we need scrollbar
-});
