f8e8813c5a95bebd381329590f9ee80f635f9d52
kubolion <144912772+kubolion@users.noreply.github.com>
2023-11-24 19:19:37 +0300

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,21 +1,25 @@
 const swiper = new Swiper(".swiper", {
-    // Optional parameters
   
-    slidesPerView: 3,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
     spaceBetween: -20,
     loop: true,
   
-    // If we need pagination
-    // pagination: {
-    //   el: ".swiper-pagination",
-    // },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  
-    // And if we need scrollbar
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+
+      851: {
+        slidesPerView: 3,
+        spaceBetween: -20
+      }
+    }
+
+
   });
   
