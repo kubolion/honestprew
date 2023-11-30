f21c1131d797af5f015db49f4c795562e30f8576
kubolion <144912772+kubolion@users.noreply.github.com>
2023-11-30 16:49:59 +0300

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,3 +1,18 @@
+document.querySelectorAll('.read-more-container').forEach(function(container) {
+  const targetId = container.getAttribute('data-target');
+  const additionalContent = container.querySelector('.additional-content');
+  const readMoreBtn = container.querySelector('.read-more-btn');
+
+  readMoreBtn.addEventListener('click', function() {
+    if (additionalContent.style.display === 'none' || additionalContent.style.display === '') {
+      additionalContent.style.display = 'block';
+      this.innerHTML = 'Read Less';
+    } else {
+      additionalContent.style.display = 'none';
+      this.innerHTML = 'Read More';
+    }
+  });
+});
 const swiper = new Swiper(".swiper", {
   
   navigation: {
