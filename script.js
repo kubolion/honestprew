5cff4f0b8731831e2499b0266015d6626baf2079
kubolion <144912772+kubolion@users.noreply.github.com>
2023-12-06 17:27:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ menuBtn.addEventListener("click", () => {
   menuMobile.classList.toggle("menu--open");
 });
 
+function makeCall() {
+  // Замените номер телефона на нужный
+  const phoneNumber = '+12799005599';
 
+  // Формируем URL для звонка
+  const telUrl = 'tel:' + phoneNumber;
 
-
+  // Открываем URL в новом окне (может вызвать звонок на мобильных устройствах)
+  window.open(telUrl, '_blank');
+}
